53e5c57077a7ac0290a32db3ad5554624e977bce
Colin Cibis <colin.cibis@gmx.de>
2024-03-19 17:41:19 +0100

    implementiert: Split Attribute

--- src/pack/SplitAttribute.java
+++ src/pack/SplitAttribute.java
@@ -1,5 +1,10 @@
 package pack;
 
+import data.DataPoint;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Class that holds a split criteria for a node of a decision tree. It contains
  * the name of the attribute and the value the split is executed on. It can
@@ -7,7 +12,7 @@ package pack;
  * values of a split criteria is Double
  */
 
-public class SplitAttribute {
+public class SplitAttribute implements SplitAttributeMethods {
 	private String attrName;
 	private Double value;
 
@@ -27,4 +32,40 @@ public class SplitAttribute {
 		this.value = value;
 	}
 
+	@Override
+	public boolean isPureDataset(List<DataPoint> dataPoints) {
+		String dataClass = dataPoints.get(0).getPredefClass();
+		for (DataPoint dp : dataPoints) {
+			if (!dp.getPredefClass().equals(dataClass)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public List<DataPoint> getLeftDataset(List<DataPoint> dataset) {
+		List<DataPoint> leftSet = new ArrayList<>();
+
+		for (DataPoint dp : dataset) {
+			Double valueForAttribute = dp.getValueForAttribute(attrName);
+			if (valueForAttribute != null && valueForAttribute <= value) {
+				leftSet.add(dp);
+			}
+		}
+		return leftSet;
+	}
+
+	@Override
+	public List<DataPoint> getRightDataset(List<DataPoint> dataset) {
+		List<DataPoint> rightSet = new ArrayList<>();
+
+		for (DataPoint dp : dataset) {
+			Double valueForAttribute = dp.getValueForAttribute(attrName);
+			if (valueForAttribute != null && valueForAttribute > this.value) {
+				rightSet.add(dp);
+			}
+		}
+		return rightSet;
+	}
 }
